a755aa29ad758f45f8f66977addec399aa19c199
NEOFLEX\isoldatenko <isoldatenko>
2021-03-24 17:29:39 +0300

    102

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import * as styles from './stories/shared/styles';
+import * as global from './stories/shared/global';
+import * as animation from './stories/shared/animation';
+import * as icons from './stories/shared/icons';
+
+export { styles, global, animation, icons };
+
+export * from './stories/Avatar';
+export * from './stories/Badge';
+export * from './stories/Button';
+export * from './stories/Icon';
+export * from './stories/Link';
